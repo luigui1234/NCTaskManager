02a5465f37727c74090d976a4853374103227cb4
Luis <xtremeluigui@hotmail.com>
2021-11-03 18:11:08 -0600

    First commit

---
+++ src/main/java/mx/edu/j2se/rocha/tasks/Main.java
@@ -0,0 +1,10 @@
+package mx.edu.j2se.rocha.tasks;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Ahhh perrooo");
+	}
+	
+}
